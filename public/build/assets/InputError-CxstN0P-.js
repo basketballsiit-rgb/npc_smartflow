c44013f75124e4334d308ee04fd614d9d6de5664
basketballsiit-rgb <nipon@example.com>
2026-09-01 15:58:29 +0700

    Auto-commit: updates at 1/9/2569 15:58:29

---
+++ public/build/assets/InputError-CxstN0P-.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-CV4Ke7L9.js";function o({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:"text-sm text-red-600 dark:text-red-400 "+t,children:r}):null}export{o as I};
