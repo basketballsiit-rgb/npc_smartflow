2f6ce72228fe0868286e51af294048f0f82b58ba
basketballsiit-rgb <nipon@example.com>
2026-09-01 15:04:19 +0700

    Auto-commit: updates at 1/9/2569 15:04:19

--- resources/js/Pages/Projects/Edit.jsx
+++ resources/js/Pages/Projects/Edit.jsx
@@ -1,3 +1,4 @@
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head, useForm, Link, usePage, router } from '@inertiajs/react';
 import { useState } from 'react';
 import Swal from 'sweetalert2';
